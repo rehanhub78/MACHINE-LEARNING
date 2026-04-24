62590fdd8377f761376fb68a676fbb5d2b9d62a3
rehanhub78 <rehankhansword@gmail.com>
2026-04-24 16:02:15 +0530

    24-04-2026

---
+++ 1. PYTHON CODE/3.RECURSION (THINKING IN SELF-REFERENCE)/2.Number-based Recursive Thinking/7.Question.py
@@ -0,0 +1,18 @@
+'''
+Print digits of a number in words recursively (e.g., 123 → “one two three”).
+'''
+def digits_name(n):
+    name = ["zero","One","Two","Theree","Four","Five","Six","Seven","Eight","Nine"]
+    if n < 10:
+        return name[n]
+    else:
+        return digits_name(n // 10) + " " + name[n % 10]
+try:
+    num = int(input("Enter your number: "))
+except ValueError:
+    print("Invalid! Please enter numeric digit.")
+    exit()
+if num < 0:
+    print(f"Digits of {num} in words is: -{digits_name(abs(num))}")
+else:
+    print(f"Digits of {num} in words is: {digits_name(num)}")
