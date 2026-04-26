a55cd033b8d0192362ee4f353f0cfd089f0becdb
rehanhub78 <rehankhansword@gmail.com>
2026-04-26 17:30:26 +0530

    26-04-2026

--- 1. PYTHON CODE/3.RECURSION (THINKING IN SELF-REFERENCE)/4. String-based Recursion/3.Question.py
+++ 1. PYTHON CODE/3.RECURSION (THINKING IN SELF-REFERENCE)/4. String-based Recursion/3.Question.py
@@ -1,1 +1,13 @@
-''''''
+'''
+Count vowels in a string recursively.
+'''
+vow = ["a","e","i","o","u"]
+def vowel_count(s, count = 0):
+    if len(s) == 0:
+        return count
+    if s[0].lower() in vow:
+        count += 1
+    return vowel_count(s[1:], count)
+
+ch = str(input("Enter your string: "))
+print(f"Total number of vowels in {ch} is: {vowel_count(ch)}")
