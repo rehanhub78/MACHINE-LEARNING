88a9fdaa80ab700f249b3863cf2e99da4d3dde58
rehanhub78 <rehankhansword@gmail.com>
2026-04-27 20:52:12 +0530

    27-04-2026

--- 1. PYTHON CODE/3.RECURSION (THINKING IN SELF-REFERENCE)/4. String-based Recursion/5.Question.py
+++ 1. PYTHON CODE/3.RECURSION (THINKING IN SELF-REFERENCE)/4. String-based Recursion/5.Question.py
@@ -7,7 +7,6 @@ def remove_occurance(s,i=0):
     if s[i] == "a":
         return 'x' + remove_occurance(s, i + 1)
     return s[i] + remove_occurance(s, i + 1)
-    print()
 
 ch = str(input("Enter your string: "))
 print(f"String after witout space is: {remove_occurance(ch.lower())} ")
