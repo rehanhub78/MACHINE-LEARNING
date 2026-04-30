7335342cac7790901e56ac232e3955b6d3f88f68
rehanhub78 <rehankhansword@gmail.com>
2026-04-30 19:40:14 +0530

    logic to find numbers of perfect squares in a given range.

---
+++ 1. PYTHON CODE/4. BASIC ARRAYS (Iterative Logical Thinking)/2.Searching & Counting Logic/7.Question.py
@@ -0,0 +1,23 @@
+'''
+Count how many elements are perfect squares.
+'''
+n = int(input("Enter how many numbers you want to store in the array: "))
+if n <= 0:
+    print("Invalid! Please enter a positive integer.")
+    exit()
+    
+my_array = []
+while len(my_array) < n:
+    try:
+        num = int(input(f"Enter number {len(my_array) + 1}: "))
+        my_array.append(num)
+    except ValueError:
+        print("Invalid! Please enter numeric digit.")
+        
+perfect_square_count = 0
+for i in my_array:
+    if i > 0:
+        sqrt_i = int(i**0.5)
+        if sqrt_i * sqrt_i == i:
+            perfect_square_count += 1
+print(f"Count of perfect squares in the array: {perfect_square_count}")
