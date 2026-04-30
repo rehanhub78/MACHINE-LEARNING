e11d84e609b25329d105182520301e7108408fa9
rehanhub78 <rehankhansword@gmail.com>
2026-04-30 19:41:42 +0530

    7

--- 1. PYTHON CODE/4. BASIC ARRAYS (Iterative Logical Thinking)/2.Searching & Counting Logic/7.Question.py
+++ 1. PYTHON CODE/4. BASIC ARRAYS (Iterative Logical Thinking)/2.Searching & Counting Logic/7.Question.py
@@ -20,4 +20,7 @@ for i in my_array:
         sqrt_i = int(i**0.5)
         if sqrt_i * sqrt_i == i:
             perfect_square_count += 1
+# second method
+ps_count = [i for i in my_array if i > 0 and int(i**0.5)**2 == i]
 print(f"Count of perfect squares in the array: {perfect_square_count}")
+print(f"Count of perfect squares in the array (second method): {len(ps_count)} \n{ps_count}")
