800c8d85e4ad3601e84a890a1f68ef30e0466628
rehanhub78 <rehankhansword@gmail.com>
2026-05-01 19:20:55 +0530

    logic to create a new array containing only even elements from the given array.

---
+++ 1. PYTHON CODE/4. BASIC ARRAYS (Iterative Logical Thinking)/3. Transformation & Manipulation/2.Question.py
@@ -0,0 +1,18 @@
+'''
+Create a new array containing only even elements.
+'''
+n = int(input("Enter how many numbers you want to store in the array: "))
+if n <= 0:
+    print("Invalid! Please enter a positive integer.")
+    exit()
+    
+my_array = []
+while len(my_array) < n:
+    try:
+        num = int(input(f"Enter number {len(my_array) + 1}: "))
+        my_array.append(num)
+    except ValueError:
+        print("Invalid! Please enter numeric digit.")
+
+even_array = [i for i in my_array if i % 2 == 0]
+print(f"Array of even elements is : {even_array}")
