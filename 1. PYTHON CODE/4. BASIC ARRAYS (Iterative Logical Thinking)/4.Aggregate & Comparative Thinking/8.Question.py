573e1af25f06e1204ed724ac40366c533dac85e4
rehanhub78 <rehankhansword@gmail.com>
2026-05-04 20:26:53 +0530

    logic to Create a frequency array of numbers (count occurrence of each number).

---
+++ 1. PYTHON CODE/4. BASIC ARRAYS (Iterative Logical Thinking)/4.Aggregate & Comparative Thinking/8.Question.py
@@ -0,0 +1,24 @@
+'''
+Create a frequency array of numbers (count occurrence of each number).
+'''
+n = int(input("Enter how many numbers you want to store in the array: "))
+if n <= 0:
+    print("Invalid! Please enter a positive integer.")
+    exit()  
+array = []
+while len(array) < n:
+    try:
+        num = int(input(f"Enter number {len(array) + 1}: "))
+        array.append(num)
+    except ValueError:
+        print("Invalid! Please enter numeric digit.")
+frequency = {}
+for num in array:
+    if num in frequency:
+        frequency[num] += 1
+    else:
+        frequency[num] = 1
+print(frequency)
+print("Frequency of each number:")
+for num, count in frequency.items():
+    print(f"{num}: {count}")
