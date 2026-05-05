f7ca8181f55c16d7c618709a405101a2260023a1
rehanhub78 <rehankhansword@gmail.com>
2026-05-05 22:34:04 +0530

    logic to find second smallest element in an array.

---
+++ 1. PYTHON CODE/4. BASIC ARRAYS (Iterative Logical Thinking)/5.Logical & Applied Array Problems/4.Question.py
@@ -0,0 +1,24 @@
+'''
+Find the second smallest element in an array.
+'''
+n = int(input("Enter how many numbers you want to store in the array: "))
+if n <= 0:
+    print("Invalid! Please enter a positive integer.")
+    exit()  
+array = []
+while len(array) < n:
+    try:
+        num = int(input(f"Enter number {len(array) + 1}: "))
+        array.append(num)
+    except ValueError:
+        print("Invalid! Please enter numeric digit.")
+if n == 1:
+    print("There is no second smallest element in the array.")
+else:
+    sorted_array = []
+    array1 = array.copy()
+    while len(array1) > 0:
+        i = min(array1)
+        sorted_array.append(i)
+        array1.remove(i)
+    print(f"The second smallest element in the array is: {sorted_array[1]}")
