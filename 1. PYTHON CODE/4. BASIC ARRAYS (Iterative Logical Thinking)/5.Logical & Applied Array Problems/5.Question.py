1ea50d03da88aeae4e5ec7a6d560a2aa9ed458a5
rehanhub78 <rehankhansword@gmail.com>
2026-05-06 19:57:15 +0530

    logic to find difference of largest and smallest element in an array.

---
+++ 1. PYTHON CODE/4. BASIC ARRAYS (Iterative Logical Thinking)/5.Logical & Applied Array Problems/5.Question.py
@@ -0,0 +1,18 @@
+'''
+Find the difference between the largest and smallest element.
+'''
+n = int(input("Enter how many numbers you want to store in the array: "))
+if n <= 0:
+    print("Invalid! Please enter a positive integer.")
+    exit()  
+array = []
+while len(array) < n:
+    try:
+        num = int(input(f"Enter number {len(array) + 1}: "))
+        array.append(num)
+    except ValueError:
+        print("Invalid! Please enter numeric digit.")
+if n == 1:
+    print("There is only one element in the array.")
+else:
+    print(f"The difference between the largest and smallest element in the array is: {max(array) - min(array)}")
