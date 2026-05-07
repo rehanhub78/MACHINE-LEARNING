3427e3e9400a6f001cce83ce8e285ba6de51a314
rehanhub78 <rehankhansword@gmail.com>
2026-05-07 20:38:12 +0530

    logic to input a string and print its length.

---
+++ 1. PYTHON CODE/5.Strings (Basic Logic Building/1. Basic String Handling/1.Question.py
@@ -0,0 +1,6 @@
+'''
+Take a string input and print its length.
+'''
+input_string = str(input("Enter a string: "))
+length_of_string = len(input_string)
+print("The length of the string is:", length_of_string)
