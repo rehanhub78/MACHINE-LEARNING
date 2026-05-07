1167b7feb45d198f51b47a421c3d44c8c49e00e0
rehanhub78 <rehankhansword@gmail.com>
2026-05-07 20:56:34 +0530

    logic to find number of characters in a string(excluding spaces).

---
+++ 1. PYTHON CODE/5.Strings (Basic Logic Building/1. Basic String Handling/5.Question.py
@@ -0,0 +1,12 @@
+'''
+Count how many characters (excluding spaces) are in the string.
+'''
+input_string = input("Enter a string: ")
+if input_string:
+    char_count = 0
+    for char in input_string:
+        if char != " ":
+            char_count += 1
+    print("The number of characters (excluding spaces) is:", char_count)
+else:
+    print("The string is empty.")
