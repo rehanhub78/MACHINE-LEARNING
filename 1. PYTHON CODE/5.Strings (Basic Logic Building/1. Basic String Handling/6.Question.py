34f4d7b622c6479edef5d386323016048209a483
rehanhub78 <rehankhansword@gmail.com>
2026-05-08 16:22:43 +0530

    logic to find the number of words in a sentence. The sentence is "Hello, how are you doing today?".

---
+++ 1. PYTHON CODE/5.Strings (Basic Logic Building/1. Basic String Handling/6.Question.py
@@ -0,0 +1,11 @@
+'''
+Count how many words are in a sentence.
+'''
+sentence = "Hello, how are you doing today?"
+# Split the sentence into words using space as a delimiter
+words = sentence.split()
+# Count the number of words
+word_count = len(words)
+print("Number of words in the sentence:", word_count)
+
+print(words)
