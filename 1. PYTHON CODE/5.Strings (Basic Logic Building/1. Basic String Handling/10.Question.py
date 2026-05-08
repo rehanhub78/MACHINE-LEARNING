e8a3780072c0a25c4d683a93dec50a6dadad2110
rehanhub78 <rehankhansword@gmail.com>
2026-05-08 20:58:31 +0530

    logic to find if string is empty or not.

---
+++ 1. PYTHON CODE/5.Strings (Basic Logic Building/1. Basic String Handling/10.Question.py
@@ -0,0 +1,8 @@
+'''
+Check whether the string is empty or not. 
+'''
+str1 = input("Enter a string: ")
+if str1.strip():
+    print("The String is not empty.")
+else:
+    print("The String is empty or contains only spaces.")
