7ac746bc388de90fde2c92e0f2d9497094018633
rehanhub78 <rehankhansword@gmail.com>
2026-05-08 20:25:57 +0530

    logic to concatenate two  strings in python.

---
+++ 1. PYTHON CODE/5.Strings (Basic Logic Building/1. Basic String Handling/7.Question.py
@@ -0,0 +1,7 @@
+'''
+Take two strings and print them concatenated.
+'''
+str1 = input("Enter first string: ")
+str2 = input("Enter second string: ")
+result = str1 + str2
+print("Concatenated string:", result)
