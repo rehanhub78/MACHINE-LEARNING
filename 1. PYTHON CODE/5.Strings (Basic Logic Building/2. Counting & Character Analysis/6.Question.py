e8de7c9835f133dd820848f51017667da5be7fc8
rehanhub78 <rehankhansword@gmail.com>
2026-05-12 20:13:42 +0530

    logic to find occurences of a character in a string and count them.

--- 1. PYTHON CODE/5.Strings (Basic Logic Building/2. Counting & Character Analysis/6.Question.py
+++ 1. PYTHON CODE/5.Strings (Basic Logic Building/2. Counting & Character Analysis/6.Question.py
@@ -1,15 +1,12 @@
 '''
-Count how many elements are common between two arrays.
+Count how many times a given character appears in a string.
 '''
-def common(s1,s2):
-    count =0
-    visited = []
-    for i in s1:
-        if i in s2 and i not in visited:
+def count_character(string, character):
+    count = 0
+    for char in string:
+        if char == character:
             count += 1
-            visited.append(i)
     return count
-
-n1 = input("Enter the first string: ")
-n2 = input("Enter the second string: ")
-print(f"Number of common elements: {common(n1, n2)}")
+string = input("Enter a string: ")
+character = input("Enter a character to count: ")  
+occurrences = count_character(string, character)
