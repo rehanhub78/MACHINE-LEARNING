3c86dbda181796f244bb850b288fb3e080a4b0a7
rehanhub78 <rehankhansword@gmail.com>
2026-05-14 11:57:57 +0530

    some changes

--- 1. PYTHON CODE/1.CONDITIONAL STATEMENT/4.Logical Operators & Compound Statements/2.Question.py
+++ 1. PYTHON CODE/1.CONDITIONAL STATEMENT/4.Logical Operators & Compound Statements/2.Question.py
@@ -4,4 +4,6 @@ Take three numbers and print the median value (neither maximum nor minimum).
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 c = int(input("Enter third number: "))
-print("The median value of",a,",",b,",",c,",","is =",(a+b+c)/3)
+s = [a,b,c]
+s.sort()
+print("The median value of",a,",",b,",",c,",","is =",s[1])
