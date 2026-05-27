a08d088ac2aa5e52273fc604931237413b2c9922
rehanhub78 <rehankhansword@gmail.com>
2026-05-27 20:09:45 +0530

    logic to find the factorial of a number using recursion.

---
+++ 1. PYTHON CODE/6.Mixed Logical Challenges/1.Number-Based Logical Combinations/5.Question.py
@@ -0,0 +1,10 @@
+'''
+Find the factorial of a number using recursion.
+'''
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+number = int(input("Enter a number: "))
+print(f"Factorial of {number} is {factorial(number)}")
